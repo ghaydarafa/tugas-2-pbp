3e756a639455422550873bdf2dd5e6033ddfb111
ghaydarafa <ghayda.rafa@gmail.com>
2022-09-11 23:38:11 +0700

    Deploy

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+...
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'item_list': data_barang_katalog,
+        'name': 'Ghayda Rafa Hernawan',
+        'student_id': '2106634332'
+    }
+    return render(request, "katalog.html", context)
