16c5d61588afcbb174c28e2caa067157a4a03967
ghaydarafa <ghayda.rafa@gmail.com>
2022-10-06 11:08:19 +0700

    Tugas 5

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-
+from .models import Task
 class CreateTaskForm(forms.Form):
-    title = forms.CharField(label='Task Title', max_length=100)
-    description = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"placeholder": "Task title", 'class':'form-control'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={"placeholder": "Task description", 'class':'form-control'}))
+
+    class Meta:
+        model = Task
+        fields = ['title', 'description']
